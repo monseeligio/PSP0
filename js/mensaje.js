34cb27d9e42dcad217653c94ab6af18f9dbff511
monseeligio <71795808+monseeligio@users.noreply.github.com>
2022-05-17 22:16:11 -0500

    Add files via upload

---
+++ js/mensaje.js
@@ -0,0 +1,37 @@
+
+function mensaje() {
+  swal({
+    title: 'Se registro correctamente nuevo proyecto',
+    icon: 'success',
+    imageUrl: 'https://pythones.net/wp-content/uploads/2020/07/ok2-min.png',
+    imageWidth: 164,
+    imageHeight: 205,
+    padding: 10,
+    animation: true,
+  });
+  }
+
+
+  function mensaje2(){
+    swal({
+      title: 'Se guardo correctamente el registro',
+      icon: 'success',
+      imageUrl: 'https://pythones.net/wp-content/uploads/2020/07/ok2-min.png',
+      imageWidth: 164,
+      imageHeight: 205,
+      padding: 10,
+      animation: true,
+    });
+  }
+
+  function mensaje3(){
+    swal({
+      title: 'Se modifico correctamente el registro',
+      icon: 'success',
+      imageUrl: 'https://pythones.net/wp-content/uploads/2020/07/ok2-min.png',
+      imageWidth: 164,
+      imageHeight: 205,
+      padding: 10,
+      animation: true,
+    });
+  }
